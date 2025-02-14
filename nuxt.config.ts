a40448a1783950be9d5740fd6f4ae7ec8b3609ad
Alif Nuryana <alifnuryana0@gmail.com>
2025-02-15 02:58:55 +0700

    chore(nuxt config): disable auto-import for custom code & components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxtjs/html-validator'],
+  components: {
+    dirs: [],
+  },
+  imports: {
+    scan: false,
+  },
   devtools: { enabled: true },
   app: {
     head: {
